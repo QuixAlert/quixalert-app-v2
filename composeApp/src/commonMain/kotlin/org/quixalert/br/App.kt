fd2e2673e4a4ac3e2ed309a7bc124f5dc3c9dbb4
jonatas <jonatas.brito.ferreira@gmail.com>
2024-11-25 13:55:20 -0300

    feat: add initial test page

--- composeApp/src/commonMain/kotlin/org/quixalert/br/App.kt
+++ composeApp/src/commonMain/kotlin/org/quixalert/br/App.kt
@@ -1,37 +1,29 @@
 package org.quixalert.br
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.Button
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import org.jetbrains.compose.resources.painterResource
+import androidx.compose.ui.unit.sp
 import org.jetbrains.compose.ui.tooling.preview.Preview
-
-import quixalert.composeapp.generated.resources.Res
-import quixalert.composeapp.generated.resources.compose_multiplatform
+import org.quixalert.br.ui.theme.primaryGreen
 
 @Composable
 @Preview
 fun App() {
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
-                }
-            }
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Text(
+                text = "QuixAlert!",
+                fontSize = 32.sp,
+                color = primaryGreen
+            )
         }
     }
 }
