94bb21f825238f772f8ef5a962832f1a7db96165
jonatas <jonatas.brito.ferreira@gmail.com>
2024-11-25 23:22:20 -0300

    fix: update app theme imports

--- composeApp/src/commonMain/kotlin/org/quixalert/br/ui/theme/AppTheme.kt
+++ composeApp/src/commonMain/kotlin/org/quixalert/br/ui/theme/AppTheme.kt
@@ -1,13 +1,22 @@
 package org.quixalert.br.ui.theme
 
+import androidx.compose.foundation.LocalIndication
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.compositionLocalOf
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.intl.Locale
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import cafe.adriel.lyricist.Lyricist
+import cafe.adriel.lyricist.rememberStrings
+import org.quixalert.br.ui.locations.Locales
+import org.quixalert.br.ui.locations.Strings
+import org.quixalert.br.ui.locations.translations
+import androidx.compose.material.ripple
 
 private val lightColorScheme = AppColorScheme(
     background = pureWhite,
@@ -53,23 +62,38 @@ private val size = AppSize(
     small = 8.dp
 )
 
+val LocalStrings = compositionLocalOf<Lyricist<Strings>> {
+    error("No Strings provided")
+}
+
 @Composable
 fun AppTheme(
     isDarkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ) {
     val colorScheme = lightColorScheme
+    val rippleIndication = ripple()
     val typography = _appTypography()
+
+    val lyricist = rememberStrings(
+        translations = translations,
+        defaultLanguageTag = Locales.PT_BR,
+        currentLanguageTag = Locale.current.toLanguageTag()
+    )
 
     CompositionLocalProvider(
         LocalAppColorScheme provides colorScheme,
         LocalAppTypography provides typography,
         LocalAppShape provides shape,
         LocalAppSize provides size,
+        LocalIndication provides rippleIndication,
+        LocalStrings provides lyricist
     ) {
         content()
     }
 }
+
+
 
 object AppTheme {
     val colorScheme: AppColorScheme
