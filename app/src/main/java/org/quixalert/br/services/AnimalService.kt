61d8c25feabc8c1b9ee104305200438280fc50e9
Joao Victor <joaocarvoli@hotmail.com>
2024-12-30 21:58:47 -0300

    feat: creating service layer to holds business logic, in simple cases just a wrapper

---
+++ app/src/main/java/org/quixalert/br/services/AnimalService.kt
@@ -0,0 +1,30 @@
+package org.quixalert.br.services
+
+import kotlinx.coroutines.Deferred
+import org.quixalert.br.domain.model.Animal
+import org.quixalert.br.domain.repository.AnimalRepository
+
+class AnimalService(
+    private val animalRepository: AnimalRepository
+): IGenericService<Animal, String> {
+
+    override fun add(entity: Animal) {
+        animalRepository.add(entity)
+    }
+
+    override fun updateById(entityId: String, entity: Animal) {
+        animalRepository.updateById(entityId, entity)
+    }
+
+    override fun deleteById(entityId: String) {
+        animalRepository.deleteById(entityId)
+    }
+
+    override fun getAll(): Deferred<List<Animal>> {
+        return animalRepository.getAll()
+    }
+
+    override fun getById(entityId: String): Deferred<Animal?> {
+        return animalRepository.getById(entityId)
+    }
+}
