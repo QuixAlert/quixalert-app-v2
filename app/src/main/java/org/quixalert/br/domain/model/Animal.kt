ffbba96b443e45b267beee936cdcfe4ce58e8d97
Joao Victor <joaocarvoli@hotmail.com>
2024-12-30 23:44:00 -0300

    refactor: updating animal entity

--- app/src/main/java/org/quixalert/br/domain/model/Animal.kt
+++ app/src/main/java/org/quixalert/br/domain/model/Animal.kt
@@ -3,6 +3,9 @@ package org.quixalert.br.domain.model
 data class Animal(
     override var id: String = "",
     val name: String = "",
+    val image: String = "",
+    val gender: AnimalGender = AnimalGender.MALE,
+    val type: AnimalType = AnimalType.DOG,
     val age: Int = 0,
     val species: String = "",
     val size: AnimalSize = AnimalSize.MEDIUM
