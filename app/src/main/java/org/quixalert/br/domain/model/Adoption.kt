3162415617f8264ed5baad92056f9f8b046c3a4c
Joao Victor <joaocarvoli@hotmail.com>
2025-01-01 17:45:39 -0300

    refactor: updating adoption entity and creating its repository and service

--- app/src/main/java/org/quixalert/br/domain/model/Adoption.kt
+++ app/src/main/java/org/quixalert/br/domain/model/Adoption.kt
@@ -1,15 +1,9 @@
 package org.quixalert.br.domain.model
 
 data class Adoption(
-    val id: String,
-    val petName: String,
-    val petImage: String,
-    val petIcon: String,
-    val status: AdoptionStatus
-)
-
-enum class AdoptionStatus {
-    PENDING,
-    APPROVED,
-    REJECTED
-}
+    override var id: String = "",
+    val petName: String = "",
+    val petImage: String = "",
+    val petIcon: String = "",
+    val status: AdoptionStatus = AdoptionStatus.PENDING
+): BaseModel(id)
