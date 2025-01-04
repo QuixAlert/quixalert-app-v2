f44c4e66f33bed9052641022080f81d27af7da81
Joao Victor <joaocarvoli@hotmail.com>
2025-01-03 22:13:53 -0300

    refactor: creating viewmodel and integrating with firebase

--- app/src/main/java/org/quixalert/br/presentation/pages/documentsSolicitationScreen/DocumentsSolicitationScreen.kt
+++ app/src/main/java/org/quixalert/br/presentation/pages/documentsSolicitationScreen/DocumentsSolicitationScreen.kt
@@ -1,56 +1,41 @@
 package org.quixalert.br.presentation.pages.documentsSolicitationScreen
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme.colors
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
+import kotlinx.coroutines.delay
+import org.quixalert.br.domain.model.DocumentType
 import org.quixalert.br.presentation.pages.profile.IconTint
 
 @Composable
-fun DocumentsSolicitationScreen(onBackClick: () -> Unit = {}, onMenuClick: () -> Unit = {}) {
-    var selectedTypeDocument by remember { mutableStateOf("Alvará") }
-    var description by remember { mutableStateOf(TextFieldValue("")) }
-    var motivation by remember { mutableStateOf(TextFieldValue("")) }
-    var details by remember { mutableStateOf(TextFieldValue("")) }
-    var address by remember { mutableStateOf("") }
+fun DocumentsSolicitationScreen(
+    onBackClick: () -> Unit = {},
+    onMenuClick: () -> Unit = {},
+    onFormClick: () -> Unit = {},
+    viewModel: DocumentsSolicitationViewModel = hiltViewModel()
+) {
+    val uiState by viewModel.uiState.collectAsState()
 
-    val textFieldColors = TextFieldDefaults.colors(
-        focusedContainerColor = Color(0xFFEEEEEE),
-        unfocusedContainerColor = Color(0xFFEEEEEE),
-        disabledContainerColor = Color(0xFFEEEEEE),
-        focusedIndicatorColor = Color.Transparent,
-        unfocusedIndicatorColor = Color.Transparent
-    )
+    var showSuccessDialog by remember { mutableStateOf(false) }
+
+    if (uiState.submissionSuccess && !showSuccessDialog) {
+        showSuccessDialog = true
+    }
 
     Column(
         verticalArrangement = Arrangement.spacedBy(16.dp),
@@ -58,10 +43,7 @@ fun DocumentsSolicitationScreen(onBackClick: () -> Unit = {}, onMenuClick: () ->
             .padding(horizontal = 16.dp)
             .padding(bottom = 32.dp)
     ) {
-        TopBar(
-            onBackClick = onBackClick,
-            onMenuClick = onMenuClick
-        )
+        TopBar(onBackClick = onBackClick, onMenuClick = onMenuClick)
 
         Text(
             text = "Fomulário de solicitação de documentos",
@@ -72,99 +54,12 @@ fun DocumentsSolicitationScreen(onBackClick: () -> Unit = {}, onMenuClick: () ->
             modifier = Modifier.padding(top = 8.dp)
         )
 
-        Row(
-            horizontalArrangement = Arrangement.spacedBy(8.dp),
-            modifier = Modifier.fillMaxWidth(),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            listOf("Alvará", "Licitações", "Licenciamentos").forEach { label ->
-                Button(
-                    onClick = { selectedTypeDocument = label },
-                    colors = ButtonDefaults.buttonColors(
-                        containerColor = if (selectedTypeDocument == label) Color(0xFF269996) else Color(0xFFB7B7B8)
-                    ),
-                    contentPadding = PaddingValues(0.dp),
-                    modifier = Modifier
-                        .height(26.dp)
-                        .weight(1f)
-                ) {
-                    Text(
-                        text = label,
-                        fontSize = 14.sp,
-                        color = Color.White,
-                        textAlign = TextAlign.Center,
-                    )
-                }
-            }
-        }
-
-        Text(
-            text = "Descrição",
-            fontSize = 16.sp,
-            fontWeight = FontWeight.Medium
-        )
-        OutlinedTextField(
-            value = description,
-            onValueChange = { description = it },
-            placeholder = { Text("Ex: Solicitação de alvará para solicitação de construção de um prédio na rua ...") },
-            colors = textFieldColors,
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(150.dp)
-        )
-
-        Text(
-            text = "Endereço",
-            fontSize = 16.sp,
-            fontWeight = FontWeight.Medium
-        )
-        TextField(
-            value = address,
-            onValueChange = { address = it },
-            placeholder = { Text("Ex: Avenida Santos Marcos, 1219") },
-            modifier = Modifier
-                .fillMaxWidth()
-                .clip(RoundedCornerShape(8.dp)),
-            colors = textFieldColors
-        )
-
-        Text(
-            text = "Motivo da solicitação",
-            fontSize = 16.sp,
-            fontWeight = FontWeight.Medium
-        )
-        TextField(
-            value = motivation,
-            onValueChange = { motivation = it },
-            placeholder = { Text("Ex: Para realizar uma construção...") },
-            modifier = Modifier
-                .fillMaxWidth()
-                .clip(RoundedCornerShape(8.dp)),
-            colors = textFieldColors
-        )
-
-        Text(
-            text = "Detalhes Extras (opcional)",
-            fontSize = 16.sp,
-            fontWeight = FontWeight.Medium
-        )
-        TextField(
-            value = details,
-            onValueChange = { details = it },
-            placeholder = { Text("Ex: Estou solicitando para..") },
-            modifier = Modifier
-                .fillMaxWidth()
-                .clip(RoundedCornerShape(8.dp)),
-            colors = textFieldColors
-        )
+        DocumentFormFields(viewModel)
 
         Button(
-            onClick = {},
-            colors = ButtonDefaults.buttonColors(
-                Color(0xFF269996)
-            ),
-            modifier = Modifier
-                .align(Alignment.CenterHorizontally)
+            onClick = { viewModel.submitDocument() },
+            colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF269996)),
+            modifier = Modifier.align(Alignment.CenterHorizontally)
         ) {
             Text(
                 text = "Solicitar Documento",
@@ -173,6 +68,116 @@ fun DocumentsSolicitationScreen(onBackClick: () -> Unit = {}, onMenuClick: () ->
             )
         }
     }
+
+    if (showSuccessDialog) {
+        AlertDialog(
+            onDismissRequest = { showSuccessDialog = false },
+            title = { Text("Documento solicitado com sucesso!") },
+            text = { Text("Seu documento foi solicitado com sucesso. Clique em OK para continuar.") },
+            confirmButton = {
+                TextButton(onClick = {
+                    viewModel.resetSubmissionState()
+                    onFormClick()
+                    showSuccessDialog = false
+                },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(0xFF269996)
+                )) {
+                    Text("OK")
+                }
+
+            }
+        )
+    }
+}
+
+@Composable
+fun DocumentFormFields(viewModel: DocumentsSolicitationViewModel) {
+    val uiState by viewModel.uiState.collectAsState()
+
+    val textFieldColors = TextFieldDefaults.colors(
+        focusedContainerColor = Color(0xFFEEEEEE),
+        unfocusedContainerColor = Color(0xFFEEEEEE),
+        disabledContainerColor = Color(0xFFEEEEEE),
+        focusedIndicatorColor = Color.Transparent,
+        unfocusedIndicatorColor = Color.Transparent
+    )
+
+    Row(
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        modifier = Modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        DocumentType.entries.forEach { type ->
+            Button(
+                onClick = { viewModel.updateDocumentType(type) },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = if (uiState.selectedTypeDocument == type) Color(0xFF269996) else Color(0xFFB7B7B8)
+                ),
+                contentPadding = PaddingValues(0.dp),
+                modifier = Modifier.height(26.dp).weight(1f)
+            ) {
+                Text(
+                    text = type.label,
+                    fontSize = 14.sp,
+                    color = Color.White,
+                    textAlign = TextAlign.Center
+                )
+            }
+        }
+    }
+
+    Text(
+        text = "Descrição",
+        fontSize = 16.sp,
+        fontWeight = FontWeight.Medium
+    )
+    OutlinedTextField(
+        value = uiState.description,
+        onValueChange = { viewModel.updateDescription(it) },
+        placeholder = { Text("Ex: Solicitação de alvará para construção de um prédio...") },
+        colors = textFieldColors,
+        modifier = Modifier.fillMaxWidth().height(150.dp)
+    )
+
+    Text(
+        text = "Endereço",
+        fontSize = 16.sp,
+        fontWeight = FontWeight.Medium
+    )
+    TextField(
+        value = uiState.address,
+        onValueChange = { viewModel.updateAddress(it) },
+        placeholder = { Text("Ex: Avenida Santos Marcos, 1219") },
+        modifier = Modifier.fillMaxWidth().clip(RoundedCornerShape(8.dp)),
+        colors = textFieldColors
+    )
+
+    Text(
+        text = "Motivo da solicitação",
+        fontSize = 16.sp,
+        fontWeight = FontWeight.Medium
+    )
+    TextField(
+        value = uiState.motivation,
+        onValueChange = { viewModel.updateMotivation(it) },
+        placeholder = { Text("Ex: Para realizar uma construção...") },
+        modifier = Modifier.fillMaxWidth().clip(RoundedCornerShape(8.dp)),
+        colors = textFieldColors
+    )
+
+    Text(
+        text = "Detalhes Extras (opcional)",
+        fontSize = 16.sp,
+        fontWeight = FontWeight.Medium
+    )
+    TextField(
+        value = uiState.details,
+        onValueChange = { viewModel.updateDetails(it) },
+        placeholder = { Text("Ex: Estou solicitando para...") },
+        modifier = Modifier.fillMaxWidth().clip(RoundedCornerShape(8.dp)),
+        colors = textFieldColors
+    )
 }
 
 @Composable
